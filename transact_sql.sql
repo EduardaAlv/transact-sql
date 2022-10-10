311c5c74566005396de8c7189b39be64f71c0892
Eduarda Alves <65419192+EduardaAlv@users.noreply.github.com>
2022-10-10 15:22:36 -0300

    Atualização

--- transact_sql.sql
+++ transact_sql.sql
@@ -1,3 +1,3 @@
---Selecionar todas as tabelas presentes no banco de dados
+--Selecionar todas as tabelas presentes no banco de dados, é possível aplicar filtros e ordenações normalmente, urtilizando o WHERE e o ORDER BY
 
 SELECT * FROM INFORMATION_SCHEMA.TABLES;
