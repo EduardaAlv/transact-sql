19d7442f8a8d76e3c3216ba01dfc56e87df9ce61
EduardaAlv <eduardasantosalveseduarda@gmail.com>
2022-10-25 21:23:09 -0300

    Executar um comando SQL em todos os bancos de dados presentes no servidor

--- transact_sql.sql
+++ transact_sql.sql
@@ -23,3 +23,45 @@ AND I.TYPE > 0
 INNER JOIN SYS.TABLES T ON T.OBJECT_ID = I.OBJECT_ID
 GROUP BY I.NAME, T.NAME
 ORDER BY I.NAME, T.NAME
+
+
+--Executar um comando SQL em todos os bancos de dados presentes no servidor
+DECLARE @nomeBanco VARCHAR(50)
+DECLARE @sql VARCHAR(256),
+		@sql2 NVARCHAR(256)
+ 
+DECLARE cursor_banco_de_dados CURSOR FOR  
+SELECT name
+FROM master.dbo.sysdatabases
+WHERE name NOT IN ('master','model','msdb','tempdb')
+
+OPEN cursor_banco_de_dados   
+FETCH NEXT FROM cursor_banco_de_dados INTO @nomeBanco   
+
+ 
+WHILE @@FETCH_STATUS = 0   
+BEGIN   
+
+	DECLARE @IND_EXECUTAR VARCHAR(100)
+
+	SET @sql2 = '
+		IF EXISTS (SELECT 1 FROM ' + @nomeBanco + '.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = ''NOMESCHEMA'' AND TABLE_NAME = ''NOMETABELA'')
+			SET @IND_EXECUTAR_IN = ''S''
+		ELSE
+			SET @IND_EXECUTAR_IN = ''N''
+		'
+	EXEC SP_EXECUTESQL @sql2, N'@IND_EXECUTAR_IN VARCHAR(100) OUTPUT', @IND_EXECUTAR_IN = @IND_EXECUTAR OUTPUT;
+
+	IF @IND_EXECUTAR IS NOT NULL AND @IND_EXECUTAR = 'S'
+	BEGIN
+
+		SET @sql =	'SELECT * FROM ' + @nomeBanco + '.NOMETABELA'
+		EXEC(@sql)
+	END
+
+	FETCH NEXT FROM cursor_banco_de_dados INTO @nomeBanco   
+END   
+
+ 
+CLOSE cursor_banco_de_dados 
+DEALLOCATE cursor_banco_de_dados
