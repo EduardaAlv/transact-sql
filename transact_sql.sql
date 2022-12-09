f41f59e3458345d24e68829e3c68a6758ddd2841
EduardaAlv <eduardasantosalveseduarda@gmail.com>
2022-12-09 10:13:01 -0300

    Adição de consultas

--- transact_sql.sql
+++ transact_sql.sql
@@ -1,6 +1,9 @@
 -- Selecionar todas as tabelas presentes no banco de dados, é possível aplicar filtros e ordenações normalmente, urtilizando o WHERE e o ORDER BY
 SELECT * FROM INFORMATION_SCHEMA.TABLES;
 
+-- Selecionar todas as tabelas presentes no banco de dados, é possível aplicar filtros e ordenações normalmente, urtilizando o WHERE e o ORDER BY
+SELECT * FROM INFORMATION_SCHEMA.TABLES;
+
 -- Selecionar o nome e a data de criação das tabelas do banco de dados
 SELECT NAME, CREATE_DATE FROM SYS.TABLES 
 ORDER BY NAME DESC
